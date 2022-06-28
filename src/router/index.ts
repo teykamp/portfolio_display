c8797cb98516b71b0901dab250125a8c56834fcd
Thomas Eykamp <embergameslp@gmail.com>
2022-06-28 11:55:55 -0400

    Remove Vuetify Banner/Create Portfolio Display View

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,7 +17,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/portfolio',
+    name: 'portfolio',
+    
+    component: () => import('../views/PortfolioView.vue')
+  },
 ]
 
 const router = new VueRouter({

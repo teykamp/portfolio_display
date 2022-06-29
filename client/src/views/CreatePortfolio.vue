050f5ec30e8b4c91995d4237243cb285a2f817da
Yona Alexander Voss-Andreae <yonavossandreae@gmail.com>
2022-06-29 17:53:15 +0200

    building main menu

---
+++ client/src/views/CreatePortfolio.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="center">
+    <p>Create Route</p>
+    <p>Draggable Selections</p>
+    <b-button>Create</b-button>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    data: () => {
+      
+    }
+  })
+</script>
+
+<style scoped>
+  @import url('../UniversalStyles.css');
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,16 +1,20 @@
 <template>
-  <HelloWorld />
+  <div class="center" style="flex-direction: column">
+    <h1>Search Profile Here</h1>
+    <input type="text" placeholder="Name of User" style="border: 1px solid black" />
+    <h1>Want to make your own <i>Snazzy</i> portfolio? Look no further!</h1>
+    <b-button>Build Portfolio Now</b-button>
+  </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
-  import HelloWorld from '../components/HelloWorld.vue'
 
   export default Vue.extend({
     name: 'HomeView',
-
-    components: {
-      HelloWorld,
-    },
   })
 </script>
+
+<style scoped>
+  @import url('../UniversalStyles.css');
+</style>

2cfc8578e8bb946d0c3812868db2c399f4cf2381
Yona Alexander Voss-Andreae <yonavossandreae@gmail.com>
2022-06-29 21:14:47 +0200

    added component factory

---
+++ client/src/components/ComponentDisplayFactory.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+
+    <div v-if="componentType === 'Achievements'">
+      <Achievements :relevantInfo="relevantInfo" />
+    </div>
+
+    <div v-else-if="componentType === 'Projects'">
+      <Projects :relevantInfo="relevantInfo" />
+    </div>
+
+    <div v-else-if="componentType === 'Education'">
+      <Education :relevantInfo="relevantInfo" />
+    </div>
+
+    <!-- Fallback -->
+    <div v-else>
+      <p>Error, Unrecognized Component Type '{{ componentType }}' in Factory!</p  >
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Achievements from './SubComponents/AchievementsDisplay.vue'
+import Projects from './SubComponents/ProjectsDisplay.vue'
+import Education from './SubComponents/EducationDisplay.vue'
+
+export default {
+  components: {
+    Achievements,
+    Projects,
+    Education
+  },
+  props: [
+    'componentType',
+    'relevantInfo'
+  ]
+}
+</script>
+
+<style>
+
+
+</style>
+

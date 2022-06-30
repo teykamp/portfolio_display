baa3a006873f9ba4660d2f09dbb7a84902ea7d8e
Yona Alexander Voss-Andreae <yonavossandreae@gmail.com>
2022-06-30 18:22:39 +0200

    draggable activ

--- client/src/views/CreatePortfolio.vue
+++ client/src/views/CreatePortfolio.vue
@@ -4,14 +4,15 @@
     <p>Draggable Selections</p>
 
     <div>
-      <draggable>
-        <transition-group name="list" tag="ul">
-          <div v-for="components in portfolioComponents" :key="components.id">
-            {{ components }}
-          </div>
-        </transition-group>
+      <draggable v-model="portfolioComponents">
+        <TransitionGroup name="list" tag="ul">
+          <li v-for="item in portfolioComponents" :key="item.id">
+          {{ item.name }}
+          </li>
+        </TransitionGroup>
       </draggable>
     </div>
+    
 
     <b-button>Create</b-button>
   </div>
@@ -24,7 +25,15 @@
   export default Vue.extend({
     data: () => {
       return {
-        portfolioComponents: ['Projects', 'Education', 'Achievements']
+        portfolioComponents: [{id: 0, name: 'Projects'}, {id: 1, name: 'Education'}, {id: 2, name: 'Achievements'}, {id: 3, name: 'Projects'}]
+      }
+    },
+    methods: {
+    
+    },
+    watch: {
+      portfolioComponents() {
+        console.log(this.portfolioComponents[0].name)
       }
     },
     components: {
@@ -34,14 +43,10 @@
 </script>
 
 <style scoped>
-  @import url('../UniversalStyles.css');
+  /* @import url('../UniversalStyles.css'); */
+  .list-move, /* apply transition to moving elements */
   .list-enter-active,
   .list-leave-active {
     transition: all 0.5s ease;
-  }
-  .list-enter-from,
-  .list-leave-to {
-    opacity: 0;
-    transform: translateX(30px);
   }
 </style>

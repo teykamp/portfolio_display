0510b36a39e25268dfa16f63be2825c9957e1c87
Thomas Eykamp <embergameslp@gmail.com>
2022-07-04 13:09:09 -0400

    styled userNotFound & accountPrivate error pages

--- client/src/components/ErrorDisplay.vue
+++ client/src/components/ErrorDisplay.vue
@@ -1,25 +1,61 @@
 <template>
   <div>
+    <v-container
+      fill-height fluid
+      class="container">
+        <div v-if="errorType === 'no server conection'">
+          <h1>There has been an issue connecting with our servers, this may be an internet connectivity issue.</h1>
+        </div>
 
-    <div v-if="userError === 'no server conection'">
-      <h1>There has been an issue connecting with our servers, this may be an internet connectivity issue.</h1>
-    </div>
+        <div v-else-if="errorType === 'user not found'">
+          <v-alert
+          prominent
+          type="error"
+          >
+            <v-row 
+              align="center"
+              justify="center">
+              <v-col class="grow">
+                <div class="errorPadding">
+                  <h5>User Not Found</h5>
+                  Please search for another user.
+                </div>
+              </v-col>
+              <v-col class="shrink">
+                <!-- SEARCH AGAIN BUTTON -->
+                <v-btn href="/">Search Again</v-btn>
+              </v-col>
+            </v-row>
+          </v-alert>  
+        </div>
 
-    <div v-else-if="userError === 'user not found'">
-      <h1>User not Found</h1>
-    </div>
+        <div v-else-if="errorType === 'account set private'">
+          <v-alert
+            prominent
+            type="warning"
+          >
+            <v-row align="center">
+              <v-col class="grow">
+                <div class="errorPadding">
+                  This portfolio has been marked as private, contact {{ username }} to gain access!
+                </div>
+              </v-col>
+            </v-row>
+          </v-alert>
+        </div>
 
-    <div v-else-if="userError === 'account set private'">
-      <h1>This portfolio has been marked as private, contact {{ username }} to gain access!</h1>
-    </div>
-
+        <!-- fallback -->
+        <div v-else>
+          <h1>There has been an error loading the page.</h1>
+        </div>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
   props: [
-    'userError',
+    'errorType',
     'username',
   ],
   created() {
@@ -34,7 +70,13 @@ export default {
 </script>
 
 <style>
-
+.errorPadding {
+  padding-left: 20px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+}
 
 </style>
 

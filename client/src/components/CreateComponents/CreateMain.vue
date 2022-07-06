fd8d5ba4a535f9cadff8b824645f7e1f7fb7291e
Yona Alexander Voss-Andreae <yonavossandreae@gmail.com>
2022-07-06 15:34:11 +0200

    added technology input in projects create view

--- client/src/components/CreateComponents/CreateMain.vue
+++ client/src/components/CreateComponents/CreateMain.vue
@@ -9,22 +9,33 @@
       <p>When you have added all the components, and filled in all the information, we encourage you to take a peak at how it will look when it is done. Hit the preview button on the top right corner!</p>
     </div>
 
-    <div v-if="!editComponentView">
+    <div class="center" style="flex-direction: row;" v-if="!editComponentView">
 
-      <button @click="addComponent()">Add</button>
       <div class="draggable-container">
-
-        <draggable v-model="portfolioComponents" class="center">
+        <p>Component List:</p>
+        <draggable v-model="portfolioComponents" v-bind="dragOptions" :move="onMove" class="center">
           <TransitionGroup name="list" tag="ul" class="draggable-list">
-            <li v-for="item in portfolioComponents" :key="item.id" :style="`background-color: ${item.color}`" class="draggable-item" @click="toggleEditView(item.name)">
+            <li v-for="item in portfolioComponents" :key="item.id" :style="`background-color: ${item.color}`" class="draggable-item">
               {{ item.name }}
             </li>
           </TransitionGroup>
         </draggable>
+      </div>
 
+      <div class="draggable-container">
+        <p>Added Components:</p>
+        <draggable v-model="addedPortfolioComponents" v-bind="dragOptions" :move="onMove" class="center">
+          <TransitionGroup name="list" tag="ul" class="draggable-list">
+            <li v-for="item in addedPortfolioComponents" :key="item.id" :style="`background-color: ${item.color}`" class="draggable-item" @click="toggleEditView(item.name)">
+              {{ item.name }}
+            </li>
+          </TransitionGroup>
+        </draggable>
       </div>
 
     </div>
+
+    
     
     <div v-else>
 
@@ -63,17 +74,31 @@ export default Vue.extend({
   data: () => {
     return {
       portfolioComponents: [{id: 0, name: 'Projects', color: 'red'}, {id: 1, name: 'Education', color: 'yellow'}, {id: 2, name: 'Accomplishments', color: 'lightblue'}, {id: 3, name: 'Experiences', color: 'green'}],
+      addedPortfolioComponents: [],
       editComponentView: false,
       componentName: undefined,
     }
   },
   methods: {
-    addComponent() {
-      this.portfolioComponents.push({id: 5, name: 'Test', color: 'orange'})
-    },
     toggleEditView(componentName) {
       this.componentName = componentName;
       this.editComponentView = true;
+    },
+    onMove({ relatedContext, draggedContext }) {
+      const relatedElement = relatedContext.element;
+      const draggedElement = draggedContext.element;
+      return (
+        (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
+      );
+    }
+  },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 0,
+        group: "description",
+        ghostClass: "ghost"
+      };
     }
   }
 })
@@ -87,19 +112,9 @@ export default Vue.extend({
   transition: all 0.5s ease;
 }
 
-.list-enter-from,
-.list-leave-to {
-  opacity: 0;
-  transform: translateX(30px);
-}
-
-.list-leave-active {
-  position: absolute;
-}
-
 .draggable-list {
   list-style: none;
-  cursor: pointer;
+  cursor: move;
 }
 
 .draggable-container {
--- client/src/components/CreateComponents/CreateSubComponents/CreateProjects.vue
+++ client/src/components/CreateComponents/CreateSubComponents/CreateProjects.vue
@@ -3,17 +3,29 @@
 
     <button @click="addProject()">Add Project</button>
     <br>
-    <div v-for="(project, index) in projects" :key="project.id">
-      <div class="center" style="flex-direction: row;">
-        <input type="text" placeholder="Name" v-model="projects[index].name" />
-        <input type="text" placeholder="Tech Used" v-model="projects[index].technologies" />
-        <input type="text" placeholder="Deployment URL" v-model="projects[index].linkToDeploy" />
-        <input type="text" placeholder="Repo URL" v-model="projects[index].linkToRepo" />
-        <input type="text" placeholder="Desc." v-model="projects[index].description" />
-        <input type="text" placeholder="Date" v-model="projects[index].date" />
-        <button @click="removeProject(index)">x</button>
-        <!-- <button @click="test(projects[index])">Test</button> -->
+    <div v-if="!techView">
+      <div v-for="(project, index) in projects" :key="project.id">
+        <div class="center" style="width: 75vw;">
+          <input type="text" placeholder="Name" v-model="projects[index].name" />
+          <button @click="editTechUsed(project)">Add Technologies Used ({{ projects[index].technologies.length }})</button>
+          <input type="text" placeholder="Deployment URL" v-model="projects[index].linkToDeploy" />
+          <input type="text" placeholder="Repo URL" v-model="projects[index].linkToRepo" />
+          <input type="text" placeholder="Desc." v-model="projects[index].description" />
+          <input type="text" placeholder="Date" v-model="projects[index].date" />
+          <button @click="removeProject(index)">x</button>
+          <!-- <button @click="test(projects[index])">Test</button> -->
+        </div>
       </div>
+    </div>
+    
+    <div v-else>
+      <h3>Add Technologies Used{{ !projectSelected.name ? '' : ` For ${projectSelected.name}` }}</h3>
+      <button @click="projectSelected.technologies.push('')">+</button>
+      <div v-for="(tech, index) in projectSelected.technologies" :key="tech.id">
+        <input type="text" placeholder="Technology Name" v-model="projectSelected.technologies[index]">
+        <button @click="projectSelected.technologies.splice(index, 1)">x</button>
+      </div>
+      <button @click="techView = false">Return to Projects</button>
     </div>
 
     <b-button @click="$parent.editComponentView = false; emitDataToGrandparent()">Back</b-button>
@@ -31,12 +43,18 @@ export default {
   },
   data: () => {
     return {
-      projects: []
+      projects: [],
+      techView: false,
+      projectSelected: ''
     }
   },
   methods: {
     test(item) {
       console.log(item)
+    },
+    editTechUsed(project) {
+      this.projectSelected = project;
+      this.techView = true;
     },
     removeProject(index) {
       this.projects.splice(index, 1);

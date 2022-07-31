13aacd2dc600aa129d3a16e87a83876aa1f6b515
Yona Alexander Voss-Andreae <yonavossandreae@gmail.com>
2022-07-31 21:10:39 +0200

    correctly configured vuexc

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,17 +1,10 @@
-// import Vue from 'vue'
-// import Vuex from 'vuex'
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-// Vue.use(Vuex)
+Vue.use(Vuex)
 
-// export default new Vuex.Store({
-//   state: {
-//   },
-//   getters: {
-//   },
-//   mutations: {
-//   },
-//   actions: {
-//   },
-//   modules: {
-//   }
-// })
+const store = new Vuex.Store({
+  state: {
+    portfolioItem: {}
+  }
+})

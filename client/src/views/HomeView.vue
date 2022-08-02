1f042acc328fe200f484f8f8ac0ff43876c314f3
Yona Alexander Voss-Andreae <yonavossandreae@gmail.com>
2022-08-02 21:00:42 +0200

    attempting to add google sign in

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,25 +4,30 @@
     <input type="text" placeholder="Name of User" v-model="query" v-on:keyup.enter="$router.push(`/display/${query}`)" style="border: 1px solid black" />
     <h1>Want to make your own <i>snazzy</i> portfolio? Look no further!</h1>
     <b-button variant="primary" style="color: white;" v-on:click="handleCreate()">Build Portfolio Now</b-button>
+    <br>
+    <v-btn color="green" dark @click="login">sign-in with google</v-btn>
   </div>
 </template>
 
-<script lang="ts">
-  import Vue from 'vue'
+<script>
 
-  export default Vue.extend({
-    name: 'HomeView',
-    data: () => {
-      return {
-        query: ''
-      }
-    },
-    methods: {
-      handleCreate() {
-        this.$router.push("/create");
-      }
+export default {
+  name: 'HomeView',
+  data: () => {
+    return {
+      query: '',
     }
-  })
+  },
+  methods: {
+    handleCreate() {
+      this.$router.push("/create");
+    },
+    async login() {
+      const googleUser = await this.$gAuth.signIn();
+      console.log(googleUser)
+    }
+  }
+}
 </script>
 
 <style scoped>

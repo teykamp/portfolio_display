9d617882158a682fee4c0518a498fa85f506bb9b
Yona Alexander Voss-Andreae <yonavossandreae@gmail.com>
2022-08-03 21:05:09 +0200

    added content to landing page

---
+++ client/src/components/HomeComponents/BuildSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-sheet color="red" style="color: white;">
+    <v-icon x-large color="white" class="pl-5 py-5">mdi-wrench</v-icon>
+    <div class="px-7 pb-5">
+      <div style="font-weight: bold" class="text-h2">
+        Build
+      </div>
+      <v-divider></v-divider>
+      <div style="font-size: 25pt;">
+        <ul>
+          <li>Join our amazing and dedicated community of professionals.</li>
+          <li>Build your own stand-out portfolio and collaborate with others.</li>
+          <li>Create an account for free with one click!</li>
+        </ul>
+      </div>
+      <div>
+        <v-btn color="red" class="mr-2" dark>
+          <v-icon class="mr-2">mdi-google</v-icon>
+          Sign-in with google
+        </v-btn>
+      </div>
+    </div>
+  </v-sheet>
+</template>

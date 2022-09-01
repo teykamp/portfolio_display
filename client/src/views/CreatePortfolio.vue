899252ec46fa4bad705c0cfc1932b377ba4dfc3a
Yona Voss-Andreae <yona@Yonas-MacBook-Pro.local>
2022-09-01 10:46:20 -0400

    fixed bug that made desktop version not work

--- client/src/views/CreatePortfolio.vue
+++ client/src/views/CreatePortfolio.vue
@@ -10,6 +10,11 @@ import Main from '../components/CreateComponents/CreateMain.vue'
 export default {
   components: {
     Main
+  },
+  created() { 
+    window.onbeforeunload = () => {
+      return "Are you sure you want to leave? Changes may not have been saved."
+    }
   }
 }
 </script>

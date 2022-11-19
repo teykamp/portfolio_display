896225a8d42c0ef3e95ef0b8bf712d68c4325bdb
Yonava <yonavossandreae@gmail.com>
2022-11-19 17:28:33 -0500

    action-btn styling

--- client/src/components/HomeComponents/ActionButtons.vue
+++ client/src/components/HomeComponents/ActionButtons.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="action-btns">
-    Actions
+  <div class="action-btns px-10 pt-2">
+    <div class="text-h4 action-btn pr-8">build</div>
+    <div class="text-h4 action-btn px-8">explore</div>
+    <div class="text-h4 action-btn px-8">register</div>
+    <v-spacer></v-spacer>
+    <div class="text-h4 action-btn">login</div>
   </div>
 </template>
 
@@ -13,7 +17,20 @@ export default {
 <style scoped>
 .action-btns {
   position: fixed;
+  width: 100%;
   top: 0;
   z-index: 2;
+  display: flex;
+  flex-direction: row;
+}
+.action-btn {
+  font-weight: 900;
+  cursor: pointer;
+  color: rgb(229, 229, 229);
+  transition: 300ms;
+}
+.action-btn:hover {
+  text-decoration: underline;
+  color: white;
 }
 </style>
